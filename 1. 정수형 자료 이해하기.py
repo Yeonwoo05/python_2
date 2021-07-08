13e1b21ffd45aa78cb619b0a69f0de1dc92c2be7
박연우 <83764130+Yeonwoo05@users.noreply.github.com>
2021-07-09 08:37:15 +0900

    Add files via upload

---
+++ 1. 정수형 자료 이해하기.py
@@ -0,0 +1,14 @@
+int_data = 10                           # 10진수
+bin_data = 0b10                         # 2진수
+oct_data = 0o10                         # 8진수
+hex_data = 0x10                         # 16진수
+long_data = 1234567890123456789         # 긴 데이터
+
+# 파이썬에서는 데이터의 범위, 즉 최댓값과 최솟값이 존재하지 않고
+# 메모리가 허용하는 범위에서 지원 가능한 수를 사용할 수 있다 (long_data)
+
+print(int_data)
+print(bin_data)
+print(oct_data)
+print(hex_data)
+print(long_data)
